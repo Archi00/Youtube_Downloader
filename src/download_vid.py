75ad03842d5ae75fc8550bb6a4636898e558a95d
Archi00 <albert.archi@gmail.com>
2021-03-19 16:52:58 +0100

    Check if only audio or video and audio

--- src/download_vid.py
+++ src/download_vid.py
@@ -1,3 +1,14 @@
 def download(yt):
-    ys = yt.streams.get_highest_resolution()
+    check_input = False
+    while (check_input == False):
+        audio = input("Do you want just the audio? (y/n): ")
+        if audio == 'y':
+            ys = yt.streams.get_audio_only()
+            check_input = True
+        elif audio == 'n':
+            ys = yt.streams.get_highest_resolution()
+            check_input = True
+        else:
+            print("You need to choose either [y] or [n]")
+            
     ys.download('../Downloads')
